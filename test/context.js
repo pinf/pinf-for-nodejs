f639f6bace67a729622380446e33f1fa81e1f2bb
Christoph Dorn <christoph@christophdorn.com>
2013-08-09 12:27:55 -0700

    initial context loading

---
+++ test/context.js
@@ -0,0 +1,64 @@
+
+const ASSERT = require("assert");
+const PATH = require("path");
+const FS = require("fs-extra");
+const Q = require("q");
+const CONTEXT = require("../lib/context");
+
+//const MODE = "test";
+const MODE = "write";
+
+
+describe("context", function() {
+
+    var options = {
+        rootPath: PATH.join(__dirname, "assets")
+    };
+
+    [
+        {
+            program: "a",
+            package: "a",
+            env: {
+                "FOO": "BAR"
+            }
+        },
+        {
+            program: null,
+            package: "a",
+            env: {
+                "FOO": "BAR"
+            }
+        }
+    ].forEach(function(info) {
+
+        it("verify output for program '" + info.program + "' and package '" + info.package + "' given ENV '" + JSON.stringify(info.env) + "'", function(done) {
+
+            var programDescriptorPath = (info.program) ? PATH.join(options.rootPath, "programs/program-" + info.program + "/program.json") : info.program;
+            var packageDescriptorPath = (info.package) ? PATH.join(options.rootPath, "packages/package-" + info.package + "/package.json") : info.package;
+
+            options.env = info.env;
+
+            CONTEXT.context(programDescriptorPath, packageDescriptorPath, options, function(err, context) {
+                if (err) return done(err);
+
+                if (MODE === "test") {
+                    ASSERT.deepEqual(
+                        context,
+                        JSON.parse(FS.readFileSync(PATH.join(options.rootPath, "results", "context-" + info.program + "-" + info.package + ".json")))
+                    );
+                } else
+                if (MODE === "write") {
+                    FS.writeFileSync(PATH.join(options.rootPath, "results", "context-" + info.program + "-" + info.package + ".json"), JSON.stringify(context, null, 4));
+                } else {
+                    throw new Error("Unknown `MODE`");
+                }
+
+                return done();
+            });
+
+        });
+
+    });
+
+});
