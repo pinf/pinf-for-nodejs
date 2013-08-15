92f58b869314cb5b1f7d3be9b979174e7759cbbe
Christoph Dorn <christoph@christophdorn.com>
2013-08-15 12:23:45 -0700

    wip

---
+++ bin/pinf.js
@@ -0,0 +1,116 @@
+
+const PATH = require("path");
+const FS = require("fs");
+const Q = require("q");
+const COMMANDER = require("commander");
+const PINF = require("..");
+const COLORS = require("colors");
+
+COLORS.setTheme({
+    error: 'red'
+});
+
+
+PINF.main(function(context, callback) {
+
+    exports.config = function(callback) {
+		return callback(null, context.config);    	
+    }
+
+    exports.status = function(callback) {
+console.log("STATUS");
+		return callback(null, {});    	
+    }
+
+    exports.start = function (callback) {
+console.log("START");
+		return callback(null);    	
+    }
+
+    exports.stop = function (callback) {
+console.log("STOP");
+		return callback(null);    	
+    }
+
+
+    var program = new COMMANDER.Command();
+
+    program
+        .version(JSON.parse(FS.readFileSync(PATH.join(__dirname, "../package.json"))).version)
+        .option("-v, --verbose", "Show verbose progress.")
+        .option("--format <TYPE>", "Output format.");
+
+    var acted = false;
+
+    program
+        .command("info")
+        .description("Show information about program runtime")
+        .action(function() {
+            acted = true;
+            return context.getInfo(function(err, info) {
+                if (err) return callback(err);
+                if (typeof program.format === "string" && program.format.toUpperCase() === "JSON") {
+                	process.stdout.write(JSON.stringify(info, null, 4) + "\n");
+                	return;
+                }
+                console.log("ENV:".bold);
+                console.log("  CWD:", ("" + info.env.CWD).yellow);
+                console.log("Program:".bold);
+                console.log("  Path:", ("" + info.program.path).yellow);
+                console.log("  Runtime:", ("" + info.program.runtime).yellow);
+                console.log("Package:".bold);
+                console.log("  Path:", ("" + info.package.path).yellow);
+                // TODO: Log dependency tree as determined by following package declarations and directories.
+                return callback(null);
+            });
+        });
+
+    program
+        .command("start")
+        .description("Start runtime")
+        .action(function() {
+            acted = true;
+            return exports.start(callback);
+        });
+
+    program
+        .command("stop")
+        .description("Stop runtime")
+        .action(function() {
+            acted = true;
+            return exports.stop(callback);
+        });
+
+    program
+        .command("config")
+        .description("Show currently active runtime configuration")
+        .action(function() {
+            acted = true;
+            return exports.config(function(err, config) {
+                if (err) return callback(err);
+                process.stdout.write(JSON.stringify(config, null, 4) + "\n");
+                return callback(null);
+            });
+        });
+
+    program
+        .command("status")
+        .description("Show current runtime status")
+        .action(function() {
+            acted = true;
+            return exports.status(function(err, status) {
+                if (err) return callback(err);
+                process.stdout.write(JSON.stringify(status, null, 4) + "\n");
+                return callback(null);
+            });
+        });
+
+    program.parse(process.argv);
+
+    if (!acted) {
+        console.error(("ERROR: Command '" + process.argv.slice(2).join(" ") + "' not found!").error);
+        program.outputHelp();
+        return callback(true);
+    }
+
+}, module);
---
+++ test/pinf-cli.js
@@ -0,0 +1,73 @@
+
+const ASSERT = require("assert");
+const PATH = require("path");
+const FS = require("fs-extra");
+const EXEC = require("child_process").exec;
+
+
+describe("pinf-cli", function() {
+
+    it("init context", function(done) {
+        FS.writeFileSync(PATH.join(__dirname, "program.json"), JSON.stringify({
+            "boot": {
+                "package": PATH.join(__dirname, "assets/packages/pinf-cli")
+            }
+        }, null, 4));
+        return done();
+    });
+
+    it("info", function(done) {
+        return call([
+            "--format", "JSON",
+            "info"
+        ], function(err, stdout) {
+            if (err) return done(err);
+            var output = JSON.parse(stdout);
+
+            ASSERT.equal(typeof output, "object");
+            ASSERT.equal(typeof output.context, "object");
+console.log("output", output);
+
+            return done();
+        });
+    });
+
+    it("config", function(done) {
+        return call([
+            "config"
+        ], function(err, stdout) {
+            if (err) return done(err);
+            var output = JSON.parse(stdout);
+
+            ASSERT.equal(typeof output, "object");
+console.log("output", output);
+
+            return done();
+        });
+    });
+
+    it("cleanup", function() {
+        FS.unlinkSync(PATH.join(__dirname, "program.json"));
+        try { FS.unlinkSync(PATH.join(__dirname, ".program.json")); } catch(err) {}
+        try { FS.removeSync(PATH.join(__dirname, ".rt")); } catch(err) {}
+    });
+
+
+    function call(args, callback) {
+        return EXEC(PATH.join(__dirname, "../bin/pinf") + " " + args.join(" "), {
+            cwd: __dirname,
+            env: {
+                PATH: process.env.PATH,
+                PINF_PROGRAM: PATH.join(__dirname, "program.json")
+            }
+        }, function (error, stdout, stderr) {
+            if (error) {
+                console.error(stdout);
+                console.error(stderr);
+                return callback(new Error("Got error '" + error + "' while trying to call `pinf`"));
+            }
+            return callback(null, stdout);
+        });
+    }
+
+});
