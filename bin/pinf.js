576ab09900c8c70868977928024c477d33855d51
Christoph Dorn <christoph@christophdorn.com>
2013-08-16 12:51:17 -0700

    cli info & config

--- bin/pinf.js
+++ bin/pinf.js
@@ -4,6 +4,7 @@ const FS = require("fs");
 const Q = require("q");
 const COMMANDER = require("commander");
 const PINF = require("..");
+const DEEPMERGE = require("deepmerge");
 const COLORS = require("colors");
 
 COLORS.setTheme({
@@ -12,10 +13,6 @@ COLORS.setTheme({
 
 
 PINF.main(function(context, callback) {
-
-    exports.config = function(callback) {
-		return callback(null, context.config);    	
-    }
 
     exports.status = function(callback) {
 console.log("STATUS");
@@ -43,26 +40,44 @@ console.log("STOP");
     var acted = false;
 
     program
-        .command("info")
+        .command("info [path]")
         .description("Show information about program runtime")
-        .action(function() {
+        .action(function(path) {
             acted = true;
-            return context.getInfo(function(err, info) {
-                if (err) return callback(err);
-                if (typeof program.format === "string" && program.format.toUpperCase() === "JSON") {
-                	process.stdout.write(JSON.stringify(info, null, 4) + "\n");
-                	return callback(null);
-                }
-                console.log("ENV:".bold);
-                console.log("  CWD:", ("" + info.env.CWD).yellow);
-                console.log("Program:".bold);
-                console.log("  Path:", ("" + info.program.path).yellow);
-                console.log("  Runtime:", ("" + info.program.runtime).yellow);
-                console.log("Package:".bold);
-                console.log("  Path:", ("" + info.package.path).yellow);
-                // TODO: Log dependency tree as determined by following package declarations and directories.
-                return callback(null);
-            });
+            if (path) {
+                return context.getPackageInfo(PATH.resolve(path), function(err, info) {
+                    if (err) return callback(err);
+                    if (typeof program.format === "string" && program.format.toUpperCase() === "JSON") {
+                        process.stdout.write(JSON.stringify(info, null, 4) + "\n");
+                        return callback(null);
+                    }
+                    // TODO: Expand on this.
+                    console.log("ENV:".bold);
+                    console.log("  CWD:", ("" + info.env.CWD).yellow);
+                    console.log("  PINF_PACKAGE:", ("" + info.env.PINF_PACKAGE).yellow);
+                    console.log("Package:".bold);
+                    console.log("  Path:", ("" + info.package.path).yellow);
+                    console.log("  ID:", ("" + info.package.id).yellow);
+                    // TODO: Log dependency tree as determined by following package declarations and directories.
+                    return callback(null);
+                });
+            } else {
+                return context.getProgramInfo(function(err, info) {
+                    if (err) return callback(err);
+                    if (typeof program.format === "string" && program.format.toUpperCase() === "JSON") {
+                    	process.stdout.write(JSON.stringify(info, null, 4) + "\n");
+                    	return callback(null);
+                    }
+                    // TODO: Expand on this.
+                    console.log("ENV:".bold);
+                    console.log("  CWD:", ("" + info.env.CWD).yellow);
+                    console.log("Program:".bold);
+                    console.log("  Path:", ("" + info.program.path).yellow);
+                    console.log("  Runtime:", ("" + info.program.runtime).yellow);
+                    // TODO: Log dependency tree as determined by following package declarations and directories.
+                    return callback(null);
+                });
+            }
         });
 
     program
@@ -82,15 +97,43 @@ console.log("STOP");
         });
 
     program
-        .command("config")
+        .command("config [path]")
         .description("Show currently active runtime configuration")
-        .action(function() {
+        .action(function(path) {
             acted = true;
-            return exports.config(function(err, config) {
-                if (err) return callback(err);
-                process.stdout.write(JSON.stringify(config, null, 4) + "\n");
-                return callback(null);
-            });
+            if (path) {
+                return context.getPackageInfo(PATH.resolve(path), function(err, info) {
+                    if (err) return callback(err);
+                    process.stdout.write(JSON.stringify({
+                        package: {
+                            config: info.package.config
+                        }
+                    }, null, 4) + "\n");
+                    return callback(null);
+                });
+            } else {
+                return context.getProgramInfo(function(err, info) {
+                    if (err) return callback(err);
+                    var config = {};
+                    if (
+                        info.program.descriptor &&
+                        info.program.descriptor.packages
+                    ) {
+                        for (var id in info.program.descriptor.packages) {
+                            config[id] = DEEPMERGE(
+                                (info.program.descriptor.packages[id].descriptor && info.program.descriptor.packages[id].descriptor.config) || {},
+                                (info.program.descriptor.config && info.program.descriptor.config[id]) || {}
+                            );
+                        }
+                    }
+                    process.stdout.write(JSON.stringify({
+                        program: {
+                            config: config
+                        }
+                    }, null, 4) + "\n");
+                    return callback(null);
+                });
+            }
         });
 
     program
--- test/pinf-cli.js
+++ test/pinf-cli.js
@@ -5,55 +5,69 @@ const FS = require("fs-extra");
 const EXEC = require("child_process").exec;
 
 
+//const MODE = "test";
+const MODE = "write";
+
 describe("pinf-cli", function() {
 
     it("init context", function(done) {
         FS.writeFileSync(PATH.join(__dirname, "program.json"), JSON.stringify({
             "boot": {
                 "package": PATH.join(__dirname, "assets/packages/pinf-cli")
+            },
+            "config": {
+                "github.com/pinf/pinf-for-nodejs/test/assets/packages/pinf-cli": {
+                    "key2": "value2: $__DIRNAME"
+                },
+                "github.com/pinf/pinf-for-nodejs/test/assets/packages/pinf-cli/node_modules/sub": {
+                    "key2": "value2: $__DIRNAME"
+                }
             }
         }, null, 4));
         return done();
     });
 
-    it("info", function(done) {
-        return call([
+    it("info - program", function(done) {
+        return runTest("info-program", [
             "--format", "JSON",
             "info"
-        ], function(err, stdout) {
-            if (err) return done(err);
-            try {
-                var output = JSON.parse(stdout);
-            } catch(err) {
-                console.error("stdout", stdout);
-                return done(new Error("Error '" + err.message + "' parsing output."));
-            }
-            ASSERT.equal(typeof output, "object");
-            ASSERT.equal(typeof output.context, "object");
-
-console.log("output", output);
-
-            return done();
-        });
+        ], done);
     });
 
-    it("config", function(done) {
-        return call([
+    it("info - program package", function(done) {
+        return runTest("info-program-package", [
+            "--format", "JSON",
+            "info",
+            "assets/packages/pinf-cli"
+        ], done);
+    });
+
+    it("info - sub package", function(done) {
+        return runTest("info-sub-package", [
+            "--format", "JSON",
+            "info",
+            "assets/packages/pinf-cli/node_modules/sub"
+        ], done);
+    });
+
+    it("config - program", function(done) {
+        return runTest("config-program", [
             "config"
-        ], function(err, stdout) {
-            if (err) return done(err);
-            try {
-                var output = JSON.parse(stdout);
-            } catch(err) {
-                console.error("stdout", stdout);
-                return done(new Error("Error '" + err.message + "' parsing output."));
-            }
-            ASSERT.equal(typeof output, "object");
+        ], done);
+    });
 
-console.log("output", output);
+    it("config - program package", function(done) {
+        return runTest("config-program-package", [
+            "config",
+            "assets/packages/pinf-cli"
+        ], done);
+    });
 
-            return done();
-        });
+    it("config - sub package", function(done) {
+        return runTest("config-sub-package", [
+            "config",
+            "assets/packages/pinf-cli/node_modules/sub"
+        ], done);
     });
 
     it("cleanup", function() {
@@ -61,6 +75,35 @@ console.log("output", output);
         try { FS.unlinkSync(PATH.join(__dirname, ".program.json")); } catch(err) {}
         try { FS.removeSync(PATH.join(__dirname, ".rt")); } catch(err) {}
     });
+
+
+    function runTest(name, args, done) {
+        return call(args, function(err, stdout) {
+            if (err) return done(err);
+            try {
+                var output = JSON.parse(stdout);
+            } catch(err) {
+                console.error("stdout", stdout);
+                return done(new Error("Error '" + err.message + "' parsing output."));
+            }
+            ASSERT.equal(typeof output, "object");
+            var json = JSON.stringify(output);
+            json = json.replace(new RegExp((process.cwd()+"/").replace(/([\/\+])/g, "\\$1"), "g"), "");
+            output = JSON.parse(json);
+            if (MODE === "test") {
+                ASSERT.deepEqual(
+                    output,
+                    JSON.parse(FS.readFileSync(PATH.join(__dirname, "assets/results", "pinf-cli-" + name + ".json")))
+                );
+            } else
+            if (MODE === "write") {
+                FS.outputFileSync(PATH.join(__dirname, "assets/results", "pinf-cli-" + name + ".json"), JSON.stringify(output, null, 4));
+            } else {
+                throw new Error("Unknown `MODE`");
+            }
+            return done();
+        });
+    }
 
 
     function call(args, callback) {
